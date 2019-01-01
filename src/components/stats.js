4d34810d475ecbddf8c10e856dffe3323f7f6c83
Scott Ian <sim2487@gmail.com>
2019-01-01 09:16:01 -0800

    initial commit

---
+++ src/components/stats.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default class Stats extends React.Component {
+  constructor(props) {
+    super(props)
+
+  }
+  render() {
+
+
+    return (
+      <h1> Hello there</h1>
+    )
+  }
+
+}
